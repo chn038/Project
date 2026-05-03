4c8518bd91a221c0a4d97b8ae6900bc7a26e4f1c
chn038 <chenhero940830@gmail.com>
2026-05-03 15:26:16 +0800

    add basic training step(wip)

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ app = marimo.App()
 
 with app.setup:
     import random
+    import re
+    from itertools import cycle
     import marimo as mo
     import torch
+    from torch.nn.functional import cross_entropy
     from torch.utils.data.datapipes.iter.combinatorics import ShufflerIterDataPipe
     from torch.utils.data.datapipes.iter import IterableWrapper
     from torch.utils.data import DataLoader
+    from torch.optim import AdamW
     from tqdm import tqdm
     from transformers import (
         AutoTokenizer,
         AutoModelForCausalLM,
         pipeline,
         LlamaConfig,
+        get_linear_schedule_with_warmup,
     )
     from matplotlib import pyplot as plt
     from Gemma3InfiniAttention import Gemma3WithInfiniAttention
@@ -32,6 +37,9 @@ with app.setup:
 @app.class_definition
 class CONFIG:
     train_test_ratio = 0.9
+    train_step = 30000
+    test_after_step = 100
+    test_steps = 100
     buffer_size = int(1e4)
     worker_count = 4
     batch_size = 16
@@ -56,93 +64,103 @@ def _():
     return
 
 
-@app.cell
-def _(re):
-    def getModelOutput(
-        model, tokenizer, x, y, model_pipeline=None, passkey: int = 9054
-    ):
-        prompt = getTestPrompt(x, y, passkey)
+@app.function
+def getModelOutput(model, tokenizer, x, y, model_pipeline=None, passkey: int = 9054):
+    prompt = getTestPrompt(x, y, passkey)
 
-        if model_pipeline is None:
-            model.eval()
-            inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-            with torch.no_grad():
-                generated_outputs = model.generate(
-                    **inputs, max_new_tokens=40, pad_token_id=tokenizer.eos_token_id
-                )
-            decoded_outputs = tokenizer.decode(
-                generated_outputs[0][inputs["input_ids"].shape[-1] :]
+    if model_pipeline is None:
+        model.eval()
+        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+        with torch.no_grad():
+            generated_outputs = model.generate(
+                **inputs, max_new_tokens=40, pad_token_id=tokenizer.eos_token_id
             )
-            del inputs, generated_outputs
-        else:
-            decoded_outputs = model_pipeline(prompt)
+        decoded_outputs = tokenizer.decode(
+            generated_outputs[0][inputs["input_ids"].shape[-1] :]
+        )
+        del inputs, generated_outputs
+    else:
+        decoded_outputs = model_pipeline(prompt)
 
-        outputs = re.search(r"\d+", decoded_outputs)
-        output = outputs.group() if outputs is not None else None
+    outputs = re.search(r"\d+", decoded_outputs)
+    output = outputs.group() if outputs is not None else None
 
-        return output, decoded_outputs
-
-    return (getModelOutput,)
+    return output, decoded_outputs
 
 
-@app.cell
-def _(getModelOutput):
-    def passkeyRetrievalTask(
-        model, tokenizer, model_pipeline, x, y, key_length=4, test_times=100
-    ):
-        correct = 0
-        for _ in range(test_times):
-            passkey = random.randint(pow(10, key_length - 1), pow(10, key_length) - 1)
-            output, raw_output = getModelOutput(
-                model, tokenizer, x, y, model_pipeline, passkey
-            )
-            if output is not None and int(output) == passkey:
-                correct += 1
-        return
-
-    return (passkeyRetrievalTask,)
+@app.function
+def passkeyRetrievalTask(
+    model, tokenizer, model_pipeline, x, y, key_length=4, test_times=100
+):
+    correct = 0
+    for _ in range(test_times):
+        passkey = random.randint(pow(10, key_length - 1), pow(10, key_length) - 1)
+        output, raw_output = getModelOutput(
+            model, tokenizer, x, y, model_pipeline, passkey
+        )
+        if output is not None and int(output) == passkey:
+            correct += 1
+    return
 
 
 @app.cell
 def _():
-    data_blocks = [i for i in range(DATA_FILE_COUNT)]
-    random.shuffle(data_blocks)
+    _data_blocks = [i for i in range(DATA_FILE_COUNT)]
+    random.shuffle(_data_blocks)
 
     def split_by_ratio(lst, ratio):
         idx = int(len(lst) * ratio)
         return lst[:idx], lst[idx:]
 
-    train_datablock, test_datablock = split_by_ratio(
-        data_blocks, CONFIG.train_test_ratio
+    _train_datablock, _test_datablock = split_by_ratio(
+        _data_blocks, CONFIG.train_test_ratio
     )
-    train_dataset = CustomDataset(train_datablock)
-    test_dataset = CustomDataset(test_datablock)
-    shuffled_train_dataset = ShufflerIterDataPipe(
-        IterableWrapper(train_dataset), buffer_size=CONFIG.buffer_size
+    _train_dataset = CustomDataset(_train_datablock)
+    _test_dataset = CustomDataset(_test_datablock)
+    _shuffled_train_dataset = ShufflerIterDataPipe(
+        IterableWrapper(_train_dataset), buffer_size=CONFIG.buffer_size
     )
-    shuffled_test_dataset = ShufflerIterDataPipe(
-        IterableWrapper(test_dataset), buffer_size=CONFIG.buffer_size
+    _shuffled_test_dataset = ShufflerIterDataPipe(
+        IterableWrapper(_test_dataset), buffer_size=CONFIG.buffer_size
     )
     train_dataloader = DataLoader(
-        shuffled_train_dataset,
+        _shuffled_train_dataset,
         batch_size=CONFIG.batch_size,
         num_workers=CONFIG.worker_count,
     )
     test_dataloader = DataLoader(
-        shuffled_test_dataset,
+        _shuffled_test_dataset,
         batch_size=CONFIG.batch_size,
         num_workers=CONFIG.worker_count,
     )
-    return
+    train_dataloader = cycle(train_dataloader)
+    test_dataloader = cycle(test_dataloader)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return (tokenizer,)
 
 
 @app.function
-def training_step(model, tokenizer, dataloader, optimizer):
+def training_step(model, tokenizer, dataloader, optimizer, steps):
+    model.train()
+    for step in range(steps):
+        optimizer.zero_grad()
+        raw_text = next(dataloader)
+        inputs = tokenizer(
+            raw_text, padding=True, padding_side="left", return_tensors="pt"
+        ).to(model.device)
+        labels = inputs['input_ids']
+        labels[:, :-1] = inputs['input_ids'][:, 1:]
+        logits = model(**inputs)[0]
+        loss = cross_entropy(logits, labels)
+
+        loss.backward()
+
     pass
 
 
 @app.cell(disabled=True)
-def _(passkeyRetrievalTask, ref_model_name):
+def _():
+    """
     model_config = LlamaConfig.from_dict(
         {
             "model_type": "llama",
@@ -174,7 +192,6 @@ def _(passkeyRetrievalTask, ref_model_name):
         model_kwargs={"torch_dtype": torch.bfloat16},
         device_map="auto",
     )
-    tokenizer = AutoTokenizer.from_pretrained(ref_model_name)
     context_sizes = []
     correctnesses = []
     for i in tqdm(range(0, 1300, 10)):
@@ -185,7 +202,9 @@ def _(passkeyRetrievalTask, ref_model_name):
         context_sizes.append(inputs.input_ids.shape[1])
         correctness = passkeyRetrievalTask(None, None, model_pipeline, x, y)
         correctnesses.append(correctness)
-    return context_sizes, correctnesses, tokenizer
+    """
+    pass
+    return
 
 
 @app.cell(disabled=True)
